3d7c9c2066b5b888a57656b38b741e996ececfe4
xiaoyu.xie <xiaoyuxie.vico@gmail.com>
2023-10-19 23:57:35 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import Labs from "./Labs";
-import HelloWorld from "./Labs/a3/HelloWorld";
-import Kanbas from "./Kanbas";
-import {HashRouter} from "react-router-dom";
-import {Routes, Route, Navigate} from "react-router";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <HashRouter>
-      <div>
-        <Routes>
-          <Route path="/"         element={<Navigate to="/Labs"/>}/>
-          <Route path="/hello"    element={<HelloWorld/>}/>
-          <Route path="/Labs/*"   element={<Labs/>}/>
-          <Route path="/Kanbas/*" element={<Kanbas/>}/>
-        </Routes>
-      </div>
-    </HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
--- src/Kanbas/Database/index.js
+++
@@ -1,8 +0,0 @@
-import courses from "./courses.json";
-import modules from "./modules.json";
-import assignments from "./assignments.json";
-export default {
-  courses,
-  modules,
-  assignments,
-};
